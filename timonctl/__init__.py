2986edcbc01893e5f381dd94747d9569860f33f3
Christian Berendt <berendt@osism.tech>
2022-09-29 13:23:29 +0200

    Make the log level configurable
    Signed-off-by: Christian Berendt <berendt@osism.tech>

--- timonctl/__init__.py
+++ timonctl/__init__.py
@@ -1,7 +1,16 @@
-from dynaconf import Dynaconf
+import sys
+
+from dynaconf import Dynaconf, Validator
 from loguru import logger  # noqa F401
 
 settings = Dynaconf(
     envvar_prefix="TIMON",
     settings_files=["timon.yaml"]
 )
+settings.validators.register(
+    Validator("LOG_LEVEL", default="INFO")
+)
+settings.validators.validate_all()
+
+logger.remove()
+logger.add(sys.stderr, level=settings.log_level)
